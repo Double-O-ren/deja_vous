f3ba3bf2ce906dce6aefc52abcb296026d62695a
EC2 Default User <ec2-user@ip-172-31-32-144.us-east-2.compute.internal>
2017-08-28 04:50:08 +0000

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/*
+ * Main Javascript file for deja_vous.
+ *
+ * This file bundles all of your javascript together using webpack.
+ */
+
+// JavaScript modules
+require('jquery');
+require('font-awesome-webpack');
+require('bootstrap');
+
+// Your own code
+require('./plugins.js');
+require('./script.js');
